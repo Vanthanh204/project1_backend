e7b49dbbede7d49742b5367aedfc34326fcca722
Vanthanh204 <vanthanh20042021@gmail.com>
2025-12-15 02:06:33 +0700

    add post

--- src/routes/todo.routes.js
+++ src/routes/todo.routes.js
@@ -2,17 +2,32 @@ const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
 
-// GET all todos
+// GET: lấy tất cả todo
 router.get("/", async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
-// POST new todo
+// POST: thêm todo mới
 router.post("/", async (req, res) => {
-  const todo = new Todo(req.body);
-  await todo.save();
-  res.json(todo);
+  try {
+    const { title } = req.body;
+
+    if (!title) {
+      return res.status(400).json({ error: "Title is required" });
+    }
+
+    const todo = new Todo({ title });
+    await todo.save();
+
+    res.status(201).json(todo);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
