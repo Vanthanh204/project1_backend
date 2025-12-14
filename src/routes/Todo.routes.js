c452be32dd8e9a7f126e58a8c105d6aa5f7417eb
Vanthanh204 <vanthanh20042021@gmail.com>
2025-12-14 20:01:43 +0700

    backend stable version

--- src/routes/Todo.routes.js
+++ src/routes/Todo.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Todo = require("../models/Todo");
 
+// GET all todos
 router.get("/", async (req, res) => {
   const todos = await Todo.find();
   res.json(todos);
 });
 
+// POST new todo
 router.post("/", async (req, res) => {
   const todo = new Todo(req.body);
   await todo.save();
