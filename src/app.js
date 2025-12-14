2f3b1dbd22c82a801e64ea7ee7a6b59d66ebe4bd
Vanthanh204 <vanthanh20042021@gmail.com>
2025-12-15 01:25:59 +0700

    fix n

--- src/app.js
+++ src/app.js
@@ -1,25 +1,23 @@
 require("dotenv").config();
-console.log("MONGO_URI =", process.env.MONGO_URI);
 
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ MongoDB connected"))
-  .catch(err => console.log("❌ MongoDB error:", err));
+console.log("SERVER STARTING");
 
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-app.use("/api/todos", require("./routes/todo.routes"));
+app.get("/api/todos", (req, res) => {
+  res.json([]);
+});
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running at http://localhost:${PORT}`)
-);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server listening on port", PORT);
+});
